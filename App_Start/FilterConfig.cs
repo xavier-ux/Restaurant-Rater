dfc37db5da5d90c32caea08ef04f6e0a96e0f929
xavier-ux <xcollins90@gmail.com>
2020-08-17 10:31:29 -0400

    Add project files.

---
+++ App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace Restaurant_Rater
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
