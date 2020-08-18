8ade2426a42a95e6c287f3ffe0243c5fea08af24
xavier-ux <xcollins90@gmail.com>
2020-08-18 16:37:43 -0400

    delete function

---
+++ Controllers/RestaurantController.cs
@@ -0,0 +1,62 @@
+﻿using Restaurant_Rater.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Restaurant_Rater.Controllers
+{
+    public class RestaurantController : Controller
+    {
+        private RestaurantDbContext _db = new RestaurantDbContext();
+        // GET: Restaurant/Index
+        public ActionResult Index()
+        {
+            return View(_db.Restaurants.ToList());
+        }
+       // Get: Restaurant/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+        //Post: Restaurant/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Restaurant restaurant)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Restaurants.Add(restaurant);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(restaurant);
+        }
+        // Get: REstaurant/Delete/{id}
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Restaurant restaurant = _db.Restaurants.Find(id);
+            if (restaurant == null)
+            {
+                return HttpNotFound();
+            }
+            return View(restaurant);
+        }
+        //Post: Restaurant/Delete/{id}
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id)
+        {
+            Restaurant restaurant = _db.Restaurants.Find(id);
+            _db.Restaurants.Remove(restaurant);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
--- Models/Restaurant.cs
+++ Models/Restaurant.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 
@@ -7,5 +8,13 @@ namespace Restaurant_Rater.Models
 {
     public class Restaurant
     {
+        public int RestaurantID { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public int Rating { get; set; }
+    }
+    public class RestaurantDbContext : DbContext
+    {
+        public DbSet<Restaurant> Restaurants { get; set; }
     }
 }
